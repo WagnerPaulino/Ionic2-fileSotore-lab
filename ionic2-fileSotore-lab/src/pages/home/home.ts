db81d592a0e55669643cbbe7cf5b65069dde5c50
Wagner Paulino <wagnersantos9778@gmail.com>
2017-12-23 13:48:55 -0300

    criação de um arquivo

--- ionic2-fileSotore-lab/src/pages/home/home.ts
+++ ionic2-fileSotore-lab/src/pages/home/home.ts
@@ -8,11 +8,23 @@ import { File } from '@ionic-native/file';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public existe:boolean = false;
+  public feito='';
   constructor(public navCtrl: NavController, private file: File) {
     this.file.checkDir(this.file.dataDirectory, 'mydir')
-      .then(_ => console.log('Directory exists'))
-      .catch(err => console.log('Directory doesnt exist'));
+      .then(_ => {
+        this.existe = _;
+      })
+      .catch(err =>{
+        this.existe = false;
+      });
+    this.file.createDir(this.file.dataDirectory,"ionicTeste",true)
+      .then(data =>{
+        this.feito = 'Diretorio criado com sucesso';
+      })
+      .catch(err => {
+        this.feito = 'erro '+err;
+      });
   }
 
 }
